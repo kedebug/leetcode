160be4cf65e777a0b3684f7cbfd73b3fe59b405e
kedebug <sunweiqq@gmail.com>
2013-06-06 19:50:24 +0800

    ParlindromePartitioning

---
+++ PalindromePartitioning/PalindromePartitioning.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<string> > partition(string s) {
+		// Start typing your C/C++ solution below
+		// DO NOT write int main() function
+		ans.clear();
+		dynamic(s);
+		vector<string> v;
+		dfs(s, 0, v);
+		return ans;
+
+	}
+	void dfs(string& s, int p, vector<string>& v) {
+		if (p == s.size()) {
+			ans.push_back(v);
+			return ;
+		}
+		string temp;
+		bool flag = false;
+		for (int i = p; i < s.size(); i++) {
+			temp += s[i];
+			if (dp[p][i]) {
+				v.push_back(temp);
+				dfs(s, i+1, v);
+				v.pop_back();
+			}
+		}        
+	}
+
+	void dynamic(string& s) {
+		memset(dp, false, sizeof(dp));
+		int len = s.size();
+		for (int i = 0; i < len; i++) 
+			dp[i][i] = true, dp[i+1][i] = true;
+		for (int d = 1; d < len; d++) {
+			for (int i = 0, j = i+d; j < len; i++, j++) {
+				if (s[i] == s[j]) 
+					dp[i][j] |= dp[i+1][j-1];
+			}
+		}
+	}
+private:
+	vector<vector<string> > ans;
+	bool dp[1000][1000];
+};
---
+++ PalindromePartitioningII/PalindromePartitioningII.cpp
@@ -0,0 +1,32 @@
+const int MAXN = 2010;
+const int INFS = 0x3fffffff;
+
+class Solution {
+public:
+	int minCut(string s) {
+		// Start typing your C/C++ solution below
+		// DO NOT write int main() function
+		int n = s.size();
+		memset(dp, false, sizeof(dp));
+		for (int i = 0; i < n; i++)
+			dp[i+1][i] = dp[i][i] = true;
+		for (int d = 1; d < n; d++)
+			for (int i = 0, j = i+d; j < n; i++, j++)
+				if (s[i] == s[j]) 
+					dp[i][j] |= dp[i+1][j-1];
+		for (int i = 0; i < n; i++)
+			F[i] = INFS;
+		F[0] = 0;
+		for (int i = 1; i < n; i++)
+			for (int j = 0; j < i; j++)
+				if (dp[0][i])
+					F[i] = 0;
+				else if (dp[j+1][i])
+					F[i] = min(F[i], F[j] + 1);
+		return F[n-1];
+
+	}
+private:
+	bool dp[MAXN][MAXN];
+	int F[MAXN];
+};
