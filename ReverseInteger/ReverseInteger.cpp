f9fb00c192723fa7ed31b9eaf5a38ea19d28891c
kedebug <sunweiqq@gmail.com>
2013-06-10 17:48:36 +0800

    2013/6/10

---
+++ ReverseInteger/ReverseInteger.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        bool minus = false;
+        if (x < 0) {
+            x = -x;
+            minus = true;
+        }
+        int res = 0;
+        while (x) {
+            res *= 10;
+            res += x % 10;
+            x /= 10;
+        }
+        return minus ? (-res) : res;
+    }
+};
---
+++ StringtoInteger(atoi)/StringtoInteger(atoi).cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int atoi(const char *str) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (str == NULL)
+            return 0;
+        while (*str == ' ') {
+            str++;
+        }
+        bool minus = false;
+        if (*str == '+') {
+            str++;
+        } else if (*str == '-') {
+            str++;
+            minus = true;
+        }
+        long long int value = 0;
+        for (; *str != '\0'; str++) {
+            if ('0' <= *str && *str <= '9') {
+                value *= 10;
+                value += *str - '0';
+            } else {
+                break;
+            }
+        }
+        if (minus)
+            value = -value;
+        if (value > INT_MAX)
+            value = INT_MAX;
+        if (value < INT_MIN)
+            value = INT_MIN;
+        return (int)value;
+    }
+};
