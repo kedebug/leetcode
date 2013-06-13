fcc5017ca5b9f68bd009db5ea332417f111a3458
kedebug <sunweiqq@gmail.com>
2013-06-13 15:08:40 +0800

    dfs

---
+++ RegularExpressionMatching/RegularExpressionMatching.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function   
+
+        if (s == NULL || p == NULL) 
+            return false;
+        if (*p == '\0') 
+            return  *s == '\0';
+        
+        if (*(p+1) == '*') {
+            // notice: ".*" means repeat '.' 0 or more times
+            while ((*s != '\0' && *p == '.') || *s == *p) {
+                if (isMatch(s, p + 2))
+                    return true;
+                s += 1;
+            }
+            return isMatch(s, p + 2);
+        } 
+        else if ((*s != '\0' && *p == '.') || *s == *p) {
+            return isMatch(s + 1, p + 1);
+        }
+        return false;
+    }
+};
