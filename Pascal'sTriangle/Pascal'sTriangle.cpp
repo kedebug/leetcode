0101f525cca89d79b140c3d55269cb428d25de0e
w.sun <sunweiqq@gmail.com>
2013-06-27 21:14:34 +0800

    1

--- Pascal'sTriangle/Pascal'sTriangle.cpp
+++ Pascal'sTriangle/Pascal'sTriangle.cpp
@@ -3,10 +3,14 @@ public:
     vector<vector<int> > generate(int numRows) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
+        
         vector<vector<int>> triangle(numRows);
+        
         if (numRows == 0)
             return triangle;
+            
         triangle[0].push_back(1);
+        
         for (int i = 1; i < numRows; i++) {
             triangle[i].push_back(1);
             for (int j = 1; j < i; j++) {
