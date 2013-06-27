08410387b5934dbdb9fa3ad4c32c980b87a92701
w.sun <sunweiqq@gmail.com>
2013-06-27 21:04:44 +0800

    1

--- BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.cpp
+++ BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.cpp
@@ -3,6 +3,7 @@ public:
     int maxProfit(vector<int> &prices) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
+        
         int minval = 0x3FFFFFFF;
         int maxprofit = 0;
         for (size_t i = 0; i < prices.size(); i++) {
