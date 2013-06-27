a6c3d2d46a1e35b2f302f427c8a07a7098e083b9
w.sun <sunweiqq@gmail.com>
2013-06-27 21:20:51 +0800

    1

--- ValidPalindrome/ValidPalindrome.cpp
+++ ValidPalindrome/ValidPalindrome.cpp
@@ -3,7 +3,9 @@ public:
     bool isPalindrome(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
+        
         string alphas;
+        
         for (size_t i = 0; i < s.size(); i++) {
             if ('A' <= s[i] && s[i] <= 'Z') 
                 alphas += s[i] - 'A' + 'a';
@@ -12,9 +14,12 @@ public:
             else if ('0' <= s[i] && s[i] <= '9')
                 alphas += s[i];
         }
+        
         if (alphas.size() == 0)
             return true;
+            
         size_t beg = 0, end = alphas.size() - 1;
+        
         while (beg < end) {
             if (alphas[beg] == alphas[end]) {
                 beg += 1;
