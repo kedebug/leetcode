e318693fff2d9b63ec54a7b495c0374fa1efe981
Wei Sun <sunweiqq@gmail.com>
2014-04-13 22:07:00 +0800

    2

--- PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.cpp
+++ PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.cpp
@@ -9,26 +9,19 @@
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        TreeLinkNode *link_head = root;
-        TreeLinkNode *next_link_head = NULL;
-        
-        while (link_head) {
-            if (link_head->left == NULL) 
-                break;
-            next_link_head = link_head->left;
-            next_link_head->next = link_head->right;
-            TreeLinkNode *link_node = link_head;
-            TreeLinkNode *next_link_node = link_head->right;
-            while (link_node->next) {
-                next_link_node->next = link_node->next->left;
-                next_link_node->next->next = link_node->next->right;
-                next_link_node = link_node->next->right;
-                link_node = link_node->next;
+        if (root == NULL) {
+            return;
+        }
+        while (root->left) {
+            TreeLinkNode* node = root;
+            while (node) {
+                node->left->next = node->right;
+                if (node->next) {
+                    node->right->next = node->next->left;
+                }
+                node = node->next;
             }
-            link_head = next_link_head;
+            root = root->left;
         }
     }
 };
