d783630ef8040fc9fe1d0c1ff4ccb60380e4068f
Wei Sun <sunweiqq@gmail.com>
2014-04-13 21:01:42 +0800

    2

--- BestTimetoBuyandSellStockIII/BestTimetoBuyandSellStockIII.cpp
+++ BestTimetoBuyandSellStockIII/BestTimetoBuyandSellStockIII.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        vector<int> dp(prices.size());   
-        vector<int> rdp(prices.size());
-        int minval = 0x3fffffff;
-        int maxprofit = 0;
-        size_t i = 0;
-        for (auto iter = prices.begin(); iter != prices.end(); iter++) {
-            minval = min(minval, *iter);
-            maxprofit = max(maxprofit, *iter - minval);
-            dp[i++] = maxprofit;
+        if (prices.size() < 2) {
+            return 0;
         }
-        int maxval = 0;
-        maxprofit = 0;
-        i = prices.size() - 1;
-        for (auto iter = prices.rbegin(); iter != prices.rend(); iter++) {
-            maxval = max(maxval, *iter);
-            maxprofit = max(maxprofit, maxval - *iter);
-            rdp[i--] = maxprofit;
+        int n = prices.size();
+        vector<int> dp1(n, 0);
+        vector<int> dp2(n, 0);
+        int minval = prices[0];
+        for (int i = 1; i < n; i++) {
+            minval = min(minval, prices[i]);
+            dp1[i] = max(dp1[i-1], prices[i] - minval);
+        }
+        int maxval = prices[n-1];
+        for (int i = n - 2; i >= 0; i--) {
+            maxval = max(maxval, prices[i]);
+            dp2[i] = max(dp2[i+1], maxval - prices[i]);
         }
         int result = 0;
-        for (i = 0; i < dp.size(); i++) {
-            result = max(result, dp[i] + rdp[i]);
+        for (int i = 0; i < n; i++) {
+            result = max(result, dp1[i] + dp2[i]);
         }
         return result;
     }
