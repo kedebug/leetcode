5495cea656fbb2c2a7840112a0506aec5496d79c
Wei Sun <sunweiqq@gmail.com>
2014-04-14 15:07:11 +0800

    2

--- ConvertSortedArraytoBinarySearchTree/ConvertSortedArraytoBinarySearchTree.cpp
+++ ConvertSortedArraytoBinarySearchTree/ConvertSortedArraytoBinarySearchTree.cpp
@@ -10,27 +10,19 @@
 class Solution {
 public:
     TreeNode *sortedArrayToBST(vector<int> &num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (num.empty()) return NULL;
-        TreeNode *root = new TreeNode(0);
-        dfs(root, 0, num.size() - 1, num);
-        return root;
-        
+        return dfs(num, 0, num.size() - 1);
     }
-    void dfs(TreeNode *root, int left, int right, vector<int>& num) {
-        if (left == right) {
-            root->val = num[left];
-            root->left = root->right = NULL;
-            return;
+    TreeNode* dfs(vector<int>& num, int start, int limit) {
+        if (start > limit) {
+            return NULL;
         }
-        int mid = (left + right) / 2;
-        root->val = num[mid];
-        root->right = new TreeNode(0);
-        dfs(root->right, mid + 1, right, num);
-        if (mid != left) {
-            root->left = new TreeNode(0);
-            dfs(root->left, left, mid - 1, num);
+        if (start == limit) {
+            return new TreeNode(num[start]);
         }
+        int middle = (start + limit) / 2;
+        TreeNode* root = new TreeNode(num[middle]);
+        root->left = dfs(num, start, middle - 1);
+        root->right = dfs(num, middle + 1, limit);
+        return root;
     }
 };
