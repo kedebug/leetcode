50c27951282c35dca200f794407e592204661658
Wei Sun <sunweiqq@gmail.com>
2014-04-14 14:34:11 +0800

    2

--- FlattenBinaryTreetoLinkedList/FlattenBinaryTreetoLinkedList.cpp
+++ FlattenBinaryTreetoLinkedList/FlattenBinaryTreetoLinkedList.cpp
@@ -30,12 +30,42 @@ public:
                 tree_stack.push(node->left);
                 node->left = NULL;
             }
-            if (flatten_node == NULL)
+            if (flatten_node == NULL) {
                 flatten_node = node;
-            else {
+            } else {
                 flatten_node->right = node;
                 flatten_node = flatten_node->right;
             }
         }
+    }
+};
+
+// recursion
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        dfs(root);
+    }
+    
+    pair<TreeNode*, TreeNode*> dfs(TreeNode* root) {
+        if (root == NULL) {
+            return make_pair((TreeNode*)NULL, (TreeNode*)NULL);
+        }
+        pair<TreeNode*, TreeNode*> left = dfs(root->left);
+        pair<TreeNode*, TreeNode*> right = dfs(root->right);
+        root->left = NULL;
+        root->right = NULL;
+        if (left.first && right.first) {
+            root->right = left.first;
+            left.second->right = right.first;
+            return make_pair(root, right.second);
+        } else if (left.first) {
+            root->right = left.first;
+            return make_pair(root, left.second);
+        } else if (right.first) {
+            root->right = right.first;
+            return make_pair(root, right.second);
+        }
+        return make_pair(root, root);
     }
 };
