634689c4dab7d429ce21ddda98e3ffee0d16d930
Wei Sun <sunweiqq@gmail.com>
2014-04-14 19:51:30 +0800

    2

--- ConstructBinaryTreefromPreorderandInorderTraversal/ConstructBinaryTreefromPreorderandInorderTraversal.cpp
+++ ConstructBinaryTreefromPreorderandInorderTraversal/ConstructBinaryTreefromPreorderandInorderTraversal.cpp
@@ -9,37 +9,28 @@
  */
 class Solution {
 public:
-    TreeNode *buildTree(vector<int>& preorder, vector<int>& inorder) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        if (preorder.empty()) return NULL;
-        
-        TreeNode *root = new TreeNode(0);
-        build(root, preorder.size(), preorder, 0, inorder, 0);
-        return root;
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        if (preorder.size() == 0) {
+            return NULL;
+        }
+        return dfs(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
     }
     
-    void build(TreeNode *root, int size, vector<int>& preorder, int preLeft, vector<int>& inorder, int inLeft) {
-        if (size == 1) {
-            root->val = preorder[preLeft];
-            return ;
+    TreeNode* dfs(vector<int>& preorder, int l1, int r1, vector<int>& inorder, int l2, int r2) {
+        if (l1 > r1) {
+            return NULL;
         }
-        int sizeLeft = 0;
-        for (int i = 0; i < size; i++) {
-            if (preorder[preLeft] == inorder[i+inLeft])
-                break;
-            sizeLeft += 1;
+        if (l1 == r1) {
+            return new TreeNode(preorder[l1]);
         }
-        root->val = preorder[preLeft];
-        if (sizeLeft > 0) {
-            root->left = new TreeNode(0);
-            build(root->left, sizeLeft, preorder, preLeft + 1, inorder, inLeft);
+        TreeNode* root = new TreeNode(preorder[l1]);
+        for (int i = l2; i <= r2; i++) {
+            if (inorder[i] == preorder[l1]) {
+                root->left = dfs(preorder, l1 + 1, l1 + i - l2, inorder, l2, i - 1);
+                root->right = dfs(preorder, l1 + i - l2 + 1, r1, inorder, i + 1, r2);
+                return root;
+            }
         }
-        int sizeRight = size - sizeLeft - 1;
-        if (sizeRight > 0) {
-            root->right = new TreeNode(0);
-            build(root->right, sizeRight, preorder, preLeft + sizeLeft + 1, inorder, inLeft + sizeLeft + 1);
-        }
+        return NULL;
     }
 };
