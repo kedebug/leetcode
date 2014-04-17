b2fa0de33f30a02cb12c95b846cf3d6f64ad074f
Wei Sun <sunweiqq@gmail.com>
2014-04-17 22:09:06 +0800

    4

--- MedianofTwoSortedArrays/MedianofTwoSortedArrays.cpp
+++ MedianofTwoSortedArrays/MedianofTwoSortedArrays.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(int A[], int m, int B[], int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        if ((m + n) & 1) 
-            return (double)findKth(A, m, B, n, (m+n+1)/2);
-        else
-            return (findKth(A, m, B, n, (m+n)/2) + findKth(A, m, B, n, (m+n)/2+1)) / 2.0;
-        
-    }
-    
-    int findKth(int A[], int m, int B[], int n, int k) {
-        if (m <= 0) return B[k-1];
-        if (n <= 0) return A[k-1];
-        if (k <= 1) return min(A[0], B[0]);
-        
-        int ans;
-        if (m/2 + n/2 + 1 >= k) {
-            if (A[m/2] >= B[n/2])
-                ans = findKth(A, m/2, B, n, k);
-            else
-                ans = findKth(A, m, B, n/2, k);
-        } 
-        else {
-            if (A[m/2] >= B[n/2])
-                ans = findKth(A, m, B + n/2 + 1, n - n/2 - 1, k - n/2 - 1);
-            else
-                ans = findKth(A + m/2 + 1, m - m/2 - 1, B, n, k - m/2 - 1);
+        if ((m + n) & 1) {
+            return findKth((m + n - 1) / 2, A, 0, m - 1, B, 0, n - 1);
+        } else {
+            int x = findKth((m + n) / 2 - 1, A, 0, m - 1, B, 0, n - 1);
+            int y = findKth((m + n) / 2, A, 0, m - 1, B, 0, n - 1);
+            return (x + y) / 2.0;
         }
-        return ans;
+    }
+    int findKth(int k, int A[], int l1, int r1, int B[], int l2, int r2) {
+        if (l1 > r1) {
+            return B[l2 + k];
+        }
+        if (l2 > r2) {
+            return A[l1 + k];
+        }
+        int m1 = (l1 + r1) / 2;
+        int m2 = (l2 + r2) / 2;
+        if (A[m1] > B[m2]) {
+            if (k + 1 < m1 - l1 + 1 + m2 - l2 + 1) {
+                return findKth(k, A, l1, m1 - 1, B, l2, r2);
+            } else {
+                return findKth(k - (m2 - l2 + 1), A, l1, r1, B, m2 + 1, r2);
+            }
+        } else {
+            if (k + 1 < m1 - l1 + 1 + m2 - l2 + 1) {
+                return findKth(k, A, l1, r1, B, l2, m2 - 1);
+            } else {
+                return findKth(k - (m1 - l1 + 1), A, m1 + 1, r1, B, l2, r2);
+            }
+        }
     }
 };
