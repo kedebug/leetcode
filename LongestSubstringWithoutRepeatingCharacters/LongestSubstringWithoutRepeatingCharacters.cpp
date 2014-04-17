7952349f65a3a8913e38c0e02b8b8434ce2d0051
Wei Sun <sunweiqq@gmail.com>
2014-04-17 22:45:22 +0800

    3

--- LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
         int hash[256];
-        memset(hash, -1, sizeof(hash));
-        int maxlen = 0, count = 0;
-        for (int i = 0, j = 0; j < s.size(); j++) {
-            if (hash[s[j]] == -1) {
-                hash[s[j]] = j;
-                maxlen = max(maxlen, j - i + 1);
-            } else {
-                while (i <= hash[s[j]]) {
-                    hash[s[i]] = -1;
-                    i += 1;
-                }
-                hash[s[j]] = j;
+        for (int i = 0; i < 256; i++) {
+            hash[i] = -1;
+        }
+        int maxlen = 0;
+        int start = 0;
+        for (int limit = 0; limit < s.size(); limit++) {
+            if (hash[s[limit]] != -1) {
+                start = max(start, hash[s[limit]] + 1);
             }
+            maxlen = max(maxlen, limit - start + 1);
+            hash[s[limit]] = limit;
         }
         return maxlen;
     }
