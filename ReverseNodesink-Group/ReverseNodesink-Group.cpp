1aac8c033532b19579c6fcb06e87171318b78c3f
Wei Sun <sunweiqq@gmail.com>
2014-04-19 15:53:37 +0800

    2

--- ReverseNodesink-Group/ReverseNodesink-Group.cpp
+++ ReverseNodesink-Group/ReverseNodesink-Group.cpp
@@ -9,53 +9,52 @@
 class Solution {
 public:
     ListNode *reverseKGroup(ListNode *head, int k) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        //
-        // only travel the list for one time
-        //
-        
-        ListNode node(0);
-        node.next = head;
-        ListNode *slow = head;
-        ListNode *fast = &node;
-        
-        for (int i = 0; i < k; i++) {
-            if (fast && fast->next) fast = fast->next;
-            else return head;
+        ListNode* node = head;
+        int len = 0;
+        while (node) {
+            len++;
+            node = node->next;
+        }
+        if (k > len) {
+            return head;
         }
         
-        bool kRemaind = true;
-        bool getHead = false;
-        ListNode *newHead = NULL;
+        ListNode prevhead(0);
+        prevhead.next = head;
         
-        while (kRemaind) {          
-            ListNode *tail = slow;
-            ListNode *prev = NULL;
-            ListNode *next = NULL;
-            
-            for (int i = 0; i < k; i++) {
-                next = slow->next;
-                slow->next = prev;
-                prev = slow;
-                slow = next;
-                
-                if (fast && fast->next) 
-                    fast = fast->next;
-                else 
-                    kRemaind = false;
+        ListNode* curr = head;
+        ListNode* prev = &prevhead;
+        
+        while (curr) {
+            int count = k - 1;
+            ListNode* last = curr;
+            while (count && last) {
+                count--;
+                last = last->next;
             }
-            if (kRemaind)
-                tail->next = fast;
-            else 
-                tail->next = slow;
-            
-            if (!getHead) {
-                newHead = prev;
-                getHead = true;
+            if (count == 0 && last) {
+                ListNode* next = last->next;
+                last->next = NULL;
+                ListNode* l = reverse(curr);
+                curr->next = next;
+                prev->next = l;
+                prev = curr;
+                curr = next;
+            } else {
+                break;
             }
         }
-        return newHead;
+        return prevhead.next;
+    }
+    
+    ListNode* reverse(ListNode* head) {
+        ListNode prevhead(0);
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prevhead.next;
+            prevhead.next = head;
+            head = next;
+        }
+        return prevhead.next;
     }
 };
