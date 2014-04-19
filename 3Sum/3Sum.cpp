f442ded592624e0ade492811a2409e1565b6e011
Wei Sun <sunweiqq@gmail.com>
2014-04-19 12:42:30 +0800

    2

--- 3Sum/3Sum.cpp
+++ 3Sum/3Sum.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+    vector<vector<int> > threeSum(vector<int> &num) {
         vector<vector<int>> result;
+        if (num.empty()) {
+            return result;
+        }
         sort(num.begin(), num.end());
-        int n = num.size();
-        for (int i = 0; i < n; i++) {
-            int lf = i + 1;
-            int rt = n - 1;
-            while (lf < rt) {
-                int sum = num[i] + num[lf] + num[rt];
-                if (sum < 0) {
-                    lf += 1;
-                }
-                else if (sum > 0) {
-                    rt -= 1;
-                }
-                else {
-                    vector<int> elem;
-                    elem.push_back(num[i]);
-                    elem.push_back(num[lf]);
-                    elem.push_back(num[rt]);
-                    if (find(result.begin(), result.end(), elem) == result.end())
-                        result.push_back(elem);
-                    lf += 1;
-                    rt -= 1;
+        for (int start = 0; start < num.size(); start++) {
+            int i = start + 1;
+            int j = num.size() - 1;
+            int target = -num[start];
+            while (i < j) {
+                if (num[i] + num[j] == target) {
+                    vector<int> triple;
+                    triple.push_back(num[start]);
+                    triple.push_back(num[i]);
+                    triple.push_back(num[j]);
+                    result.push_back(triple);
+                    while (i + 1 < num.size() && num[i] == num[i+1]) {
+                        i++;
+                    }
+                    while (j - 1 >= 0 && num[j] == num[j-1]) {
+                        j--;
+                    }
+                    i++;
+                    j--;
+                } else if (num[i] + num[j] < target) {
+                    i++;
+                } else {
+                    j--;
                 }
             }
+            while (start + 1 < num.size() && num[start] == num[start+1]) {
+                start++;
+            }
         }
-        return move(result);
+        return result;
     }
 };
