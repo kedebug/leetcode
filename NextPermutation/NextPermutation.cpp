1ed8730f64ee6962b8f29e7cf4e61ff8aa2778bc
Wei Sun <sunweiqq@gmail.com>
2014-04-20 19:55:28 +0800

    3

--- NextPermutation/NextPermutation.cpp
+++ NextPermutation/NextPermutation.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        int n = num.size();
-        if (n <= 1) return;
-        
-        bool found = false;
-        int i, p, maxval = INT_MIN;
-        for (i = n - 1; i >= 1; i--) {
-            if (maxval < num[i]) {
-                maxval = num[i];
-            }
-            if (num[i-1] < maxval) {
-                int delta = INT_MAX;
-                for (int j = i; j < n; j++) {
-                    if (num[j] > num[i-1] && num[j] - num[i-1] < delta)
-                        p = j;
-                }
-                found = true; 
+    void nextPermutation(vector<int> &num) {
+        int i = num.size() - 1;
+        while (i >= 1 && num[i-1] >= num[i]) {
+            i--;
+        }
+        if (i == 0) {
+            reverse(num.begin(), num.end());
+            return;
+        }
+        for (int j = num.size() - 1; j >= i; j--) {
+            if (num[j] > num[i-1]) {
+                swap(num[j], num[i-1]);
                 break;
             }
         }
-        if (!found) {
-            reverse(num.begin(), num.end());
-        }
-        else {
-            swap(num[p], num[i-1]);
-            sort(num.begin() + i, num.end());
-        }
+        reverse(num.begin() + i, num.end());
     }
 };
