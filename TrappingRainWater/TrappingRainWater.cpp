5dc95bf510c75ba9849db8148c2a1c42ffaee321
Wei Sun <sunweiqq@gmail.com>
2014-04-21 21:18:20 +0800

    4

--- TrappingRainWater/TrappingRainWater.cpp
+++ TrappingRainWater/TrappingRainWater.cpp
@@ -1,3 +1,4 @@
+// O(n) time, O(n) space
 class Solution {
 public:
     int trap(int A[], int n) {
@@ -20,6 +21,37 @@ public:
             int min_height = min(max_from_left[i-1], max_from_right[i+1]);
             if (min_height > A[i])
                 water += min_height - A[i];
+        }
+        return water;
+    }
+};
+
+// O(n) time, O(1) space
+class Solution {
+public:
+    int trap(int A[], int n) {
+        int maxh = 0;
+        int water = 0;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            if (A[i] <= maxh) {
+                temp += maxh - A[i];
+            } else {
+                maxh = A[i];
+                water += temp;
+                temp = 0;
+            }
+        }
+        maxh = 0;
+        temp = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (A[i] < maxh) {
+                temp += maxh - A[i];
+            } else {
+                maxh = A[i];
+                water += temp;
+                temp = 0;
+            }
         }
         return water;
     }
