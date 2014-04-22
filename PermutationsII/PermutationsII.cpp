4aed337a2a74b6caae721dc05247fb404732148c
Wei Sun <sunweiqq@gmail.com>
2014-04-22 17:11:46 +0800

    3

--- PermutationsII/PermutationsII.cpp
+++ PermutationsII/PermutationsII.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
+    vector<vector<int> > permuteUnique(vector<int> &num) {
         vector<vector<int>> result;
-        
+        sort(num.begin(), num.end());
         do {
             result.push_back(num);
-            nextPermutation(num);
-        } while (!isEqual(result[0], num));
-        
-        return move(result);    
+        } while (getNext(num));
+        return result;
     }
     
-    bool isEqual(vector<int>& n1, vector<int>& n2) {
-        if (n1.size() != n2.size())
+    bool getNext(vector<int>& num) {
+        int i = num.size() - 1;
+        while (i >= 1 && num[i-1] >= num[i]) {
+            i--;
+        }
+        if (i == 0) {
             return false;
-        for (int i = 0; i < n1.size(); i++) {
-            if (n1[i] != n2[i]) return false;
         }
+        int j = num.size() - 1;
+        while (j >= i) {
+            if (num[j] > num[i-1]) {
+                break;
+            }
+            j--;
+        }
+        swap(num[i-1], num[j]);
+        reverse(num.begin() + i, num.end());
         return true;
-    }
-    
-    void nextPermutation(vector<int>& num) {
-        int lf = num.size() - 2;
-        while (lf >= 0 && num[lf] >= num[lf+1])
-            lf -= 1;
-        if (lf == -1) {
-            reverse(num.begin(), num.end());
-            return;
-        }
-        int rt = num.size() - 1;
-        while (rt > lf && num[rt] <= num[lf])
-            rt -= 1;
-        swap(num[lf], num[rt]);
-        reverse(num.begin() + lf + 1, num.end());
     }
 };
