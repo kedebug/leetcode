965ad96071c1a7364a84ca3eba675cbe00598b0e
Wei Sun <sunweiqq@gmail.com>
2014-04-23 19:34:20 +0800

    2

--- N-Queens/N-Queens.cpp
+++ N-Queens/N-Queens.cpp
@@ -1,51 +1,35 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        string row(n, '.');
-        vector<string> queens(n, row);
-        
-        vector<bool> visited_col(n, false);
-        vector<bool> visited_slope1(2*n-1, false);
-        vector<bool> visited_slope2(2*n-1, false);
-        vector<vector<bool>> visited;
-        visited.push_back(visited_col);
-        visited.push_back(visited_slope1);
-        visited.push_back(visited_slope2);
-        
+    vector<vector<string> > solveNQueens(int n) {
         vector<vector<string>> result;
-        
-        generateNQueens(queens, visited, 0, n, result);
+        if (n == 0) {
+            return result;
+        }
+        vector<string> sol(n, string(n, '.'));
+        vector<vector<bool>> visited(3, vector<bool>(2*n, false));
+        dfs(result, sol, visited, 0);
         return result;
     }
     
-    void generateNQueens(vector<string>& queens, vector<vector<bool>>& visited, 
-        int row, int n, vector<vector<string>>& result) {
-        
+    void dfs(vector<vector<string>>& result, vector<string>& sol,  vector<vector<bool>>& visited, int row) {
+        int n = sol.size();
         if (row == n) {
-            result.push_back(queens);
+            result.push_back(sol);
             return;
         }
-        
         for (int col = 0; col < n; col++) {
-            if (visited[0][col] == false && 
-                visited[1][row+col] == false &&
-                visited[2][n-1-col+row] == false) {
-                
-                queens[row][col] = 'Q';
-                visited[0][col] = true;
-                visited[1][row+col] = true;
-                visited[2][n-1-col+row] = true;
-                
-                generateNQueens(queens, visited, row + 1, n, result);
-                
-                queens[row][col] = '.';
-                visited[0][col] = false;
-                visited[1][row+col] = false;
-                visited[2][n-1-col+row] = false;
+            if (visited[0][col] || visited[1][row+col] || visited[2][n-1-row+col]) {
+                continue;
             }
+            visited[0][col] = true;
+            visited[1][row+col] = true;
+            visited[2][n-1-row+col] = true;
+            sol[row][col] = 'Q';
+            dfs(result, sol, visited, row + 1);
+            sol[row][col] = '.';
+            visited[0][col] = false;
+            visited[1][row+col] = false;
+            visited[2][n-1-row+col] = false;
         }
     }
 };
