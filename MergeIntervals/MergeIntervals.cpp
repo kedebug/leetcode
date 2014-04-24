b6c6b31d1e7ae1c2585cdd60b887383be86029c8
Wei Sun <sunweiqq@gmail.com>
2014-04-24 14:26:23 +0800

    2

--- MergeIntervals/MergeIntervals.cpp
+++ MergeIntervals/MergeIntervals.cpp
@@ -7,38 +7,31 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
- 
-bool compare(const Interval& lhs, const Interval& rhs) {
-    if (lhs.start == rhs.start)
-        return lhs.end < rhs.end;
-    return lhs.start < rhs.start;
+
+inline bool operator < (const Interval& x, const Interval& y) {
+    if (x.start == y.start) {
+        return x.end < y.end;
+    }
+    return x.start < y.start;
 }
 
 class Solution {
 public:
-    vector<Interval> merge(vector<Interval>& intervals) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        if (intervals.empty()) return vector<Interval>();
-        
-        sort(intervals.begin(), intervals.end(), compare);
-        
-        vector<Interval> merged_interval;
-        merged_interval.push_back(intervals[0]);
-        
+    vector<Interval> merge(vector<Interval> &intervals) {
+        vector<Interval> result;
+        if (intervals.empty()) {
+            return result;
+        }
+        sort(intervals.begin(), intervals.end());
+        result.push_back(intervals[0]);
         for (int i = 1; i < intervals.size(); i++) {
-            int start = merged_interval[merged_interval.size()-1].start;
-            int end = merged_interval[merged_interval.size()-1].end;
-            if (intervals[i].start <= end) {
-                end = max(end, intervals[i].end);
-                merged_interval.pop_back();
-                merged_interval.push_back(Interval(start, end));
-            }
-            else {
-                merged_interval.push_back(intervals[i]);
+            Interval& back = result.back();
+            if (back.end >= intervals[i].start) {
+                back.end = max(back.end, intervals[i].end);
+            } else {
+                result.push_back(intervals[i]);
             }
         }
-        return merged_interval;
+        return result;
     }
 };
