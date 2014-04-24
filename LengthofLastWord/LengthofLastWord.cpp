b34e422c5f2228f631203ca4019787fe561b440a
Wei Sun <sunweiqq@gmail.com>
2014-04-24 12:58:38 +0800

    1

--- LengthofLastWord/LengthofLastWord.cpp
+++ LengthofLastWord/LengthofLastWord.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(const char *s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        int last_word_length = 0;
-        int word_length = 0;
-        while (*s != '\0') {
-            while (*s == ' ')
-                s++;
-            
-            int word_length = 0;
-            while (*s != ' ' && *s != '\0') {
-                s++;
-                word_length += 1;
+        int curr = 0;
+        const char* p = s;
+        while (*p != '\0') {
+            if (*p == ' ') {
+                p++;
+            } else {
+                curr = 0;
+                while (*p != '\0' && *p != ' ') {
+                    p++;
+                    curr++;
+                }
             }
-            if (word_length)
-                last_word_length = word_length;
         }
-        return last_word_length;
+        return curr;
     }
 };
