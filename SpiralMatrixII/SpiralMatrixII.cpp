f1e3ee22549a1397da3ea8c8f748ad03cafd918a
Wei Sun <sunweiqq@gmail.com>
2014-04-24 12:44:50 +0800

    3

--- SpiralMatrixII/SpiralMatrixII.cpp
+++ SpiralMatrixII/SpiralMatrixII.cpp
@@ -1,44 +1,39 @@
-
-const int dir[4][2] = {0, 1, 1, 0, 0, -1, -1, 0}; // right, down, left, up
-
 class Solution {
 public:
     vector<vector<int> > generateMatrix(int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        if (n == 0) return vector<vector<int>>();
-        
         vector<vector<int>> matrix(n, vector<int>(n, 0));
-        
-        int x = 0, y = -1;
-        int row = n, col = n + 1;
-        bool go_horizon = true;
-        int i = 1, k = 0;
-        while (i <= n * n) {
+        int dir[4][2] = {0, 1, 1, 0, 0, -1, -1, 0};
+        int x = 0;
+        int y = -1;
+        int k = 0;
+        int count = 0;
+        int horizon = true;
+        int row = n;
+        int col = n + 1;
+        while (count < n * n) {
             int dx = dir[k%4][0];
             int dy = dir[k%4][1];
-            k += 1;
-            
-            if (go_horizon) {
-                go_horizon = false;
-                col -= 1;
-                for (int j = 0; j < col; j++, i++) {
+            k++;
+            if (horizon) {
+                horizon = false;
+                col--;
+                for (int i = 0; i < col; i++) {
                     x += dx;
                     y += dy;
-                    matrix[x][y] = i;
+                    count++;
+                    matrix[x][y] = count;
                 }
-            }
-            else {
-                go_horizon = true;
-                row -= 1;
-                for (int j = 0; j < row; j++, i++) {
+            } else {
+                horizon = true;
+                row--;
+                for (int i = 0; i < row; i++) {
                     x += dx;
                     y += dy;
-                    matrix[x][y] = i;
+                    count++;
+                    matrix[x][y] = count;
                 }
             }
         }
-        return move(matrix);
+        return matrix;
     }
 };
