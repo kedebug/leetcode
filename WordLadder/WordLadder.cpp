f11d2e85e3425ff0eb9378738d44c941c4a38602
Wei Sun <sunweiqq@gmail.com>
2014-09-20 21:18:46 +0800

    Update WordLadder.cpp

--- WordLadder/WordLadder.cpp
+++ WordLadder/WordLadder.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     int ladderLength(string start, string end, unordered_set<string> &dict) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        queue<pair<string, int>> Q;
-        unordered_set<string> visited;
-        Q.push(make_pair(start, 1));
-        
-        while (!Q.empty()) {
-            pair<string, int> src = Q.front();
-            Q.pop();
-            if (src.first == end) {
-                return src.second;
-            }
-            vector<string> neighbor;
-            getNeighbors(src.first, neighbor, dict);
-            for (string word : neighbor) {
-                if (visited.find(word) == visited.end()) {
-                    visited.insert(word);
-                    Q.push(make_pair(word, src.second + 1));
-                }
-            } 
-        }
-        return 0;
-    }
+        queue<string> q;
+        unordered_map<string, bool> visited;
+        map<string, string> steps;
 
-    void getNeighbors(const string& word, 
-        vector<string>& neighbor, 
-        const unordered_set<string>& dict) {
-            for (size_t i = 0; i < word.size(); i++) {
-                string copyword(word);
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    copyword[i] = ch;
-                    if (dict.find(copyword) != dict.end()) {
-                        neighbor.push_back(copyword);
+        q.push(start);
+        visited[start] = true;
+        while (!q.empty()) {
+            string u = q.front();
+            q.pop();
+            if (u == end) {
+                int count = 1;
+                while (steps.find(u) != steps.end()) {
+                    u = steps[u];
+                    count++;
+                }
+                return count;
+            }
+            string v = u;
+            for (int i = 0; i < v.size(); i++) {
+                for (char c = 'a'; c <= 'z'; c++) {
+                    v[i] = c;
+                    if (visited.find(v) == visited.end() 
+                        && dict.find(v) != dict.end()) {
+                        visited[v] = true;
+                        steps[v] = u;
+                        q.push(v);
                     }
                 }
+                v[i] = u[i];
             }
+        }
+        return 0;
     }
 };
