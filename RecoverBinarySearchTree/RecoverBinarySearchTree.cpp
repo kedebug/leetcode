46cec258a694a731663504e61203da529c8f8607
Wei Sun <sunweiqq@gmail.com>
2014-09-30 00:00:52 +0800

    3

--- RecoverBinarySearchTree/RecoverBinarySearchTree.cpp
+++ RecoverBinarySearchTree/RecoverBinarySearchTree.cpp
@@ -1,56 +1,44 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// O(n) time, O(1) space
 class Solution {
 public:
     void recoverTree(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        TreeNode* node = root;
+        TreeNode* prev = NULL;
+        TreeNode* prev1 = NULL;
+        TreeNode* curr1 = NULL;
+        TreeNode* prev2 = NULL;
+        TreeNode* curr2 = NULL;
         
-        TreeNode *parent = NULL;
-        TreeNode *current = root;
-        TreeNode *mistake_node1 = NULL;
-        TreeNode *mistake_node2 = NULL;
-        
-        while (current != NULL) {
-            if (current->left == NULL) {
-                if (parent && parent->val > current->val) {
-                    if (mistake_node1 == NULL) 
-                        mistake_node1 = parent;
-                    mistake_node2 = current;
+        while (node != NULL) {
+            if (node->left == NULL) {
+                prev = node;
+                node = node->right;
+            } else {
+                TreeNode* next = node->left;
+                while (next->right && next->right != node) {
+                    next = next->right;
                 }
-                parent = current;
-                current = current->right;
+                if (next->right == NULL) {
+                    next->right = node;
+                    node = node->left;
+                } else {
+                    prev = node;
+                    node = node->right;
+                    next->right = NULL;
+                }
             }
-            else {
-                TreeNode *prev = current->left;
-                while (prev->right && prev->right != current)
-                    prev = prev->right;
-                
-                if (prev->right == NULL) {
-                    prev->right = current;
-                    current = current->left;
-                }
-                else {
-                    if (parent->val > current->val) {
-                        if (mistake_node1 == NULL) 
-                            mistake_node1 = parent;
-                        mistake_node2 = current;
-                    }
-                    prev->right = NULL;
-                    parent = current;
-                    current = current->right;
+            if (prev && node && prev->val > node->val) {
+                if (prev1 == NULL) {
+                    prev1 = prev, curr1 = node;
+                } else {
+                    prev2 = prev, curr2 = node;
                 }
             }
         }
-        if (mistake_node1 && mistake_node2)
-            swap(mistake_node1->val, mistake_node2->val);
+        if (prev1 && curr2) {
+            swap(prev1->val, curr2->val);
+        } else {
+            swap(prev1->val, curr1->val);
+        }
     }
 };
-// http://fisherlei.blogspot.com/2012/12/leetcode-recover-binary-search-tree.html
